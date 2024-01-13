5510784389e184d5b1ba0bcef1a878a2529d7389
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2024-01-13 04:34:16 +0400

    custom react

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,6 +1,37 @@
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 import App from './App.jsx'
 
+const reactElement = {
+    type: "a",
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom react app</h1>
+        </div>
+    )
+}
+
+const areactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    'click to visit google'
+)
+
+const AnotherElement = (
+    <a href="http://google.com" target='_blank' rel='noreferrer'>Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    areactElement
 )
