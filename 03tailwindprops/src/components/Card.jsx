3f0219d1b744d1a42822eb4a0cf7e308740f58fd
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2024-01-13 06:02:09 +0400

    app with props

---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+const Card = (username = ' HC ', position = 'Not yet assigned') => {
+  return (
+    <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+      <img className="w-24 h-24 rounded-full mx-auto" src="https://www.werchristmas.co.uk/cdn/shop/products/1_ecc72ed9-681c-4b79-b024-dbb52d71ba3c.jpg" alt="" width="384" height="512" />
+        <div className="pt-6 te space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              {username}
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              {position}
+            </div>
+          </figcaption>
+        </div>
+    </figure>
+  )
+}
+
+Card.propTypes = {
+    username: PropTypes.string.isRequired
+}
+
+export default Card
