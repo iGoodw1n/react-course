43524434cb94c65ea09c13c9aee79a7e6f58e351
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2024-01-13 04:59:56 +0400

    counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  const [counter, setCounter] = useState(111)
+
+  const addValue = () => {
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    console.log(counter);
+  }
+
+  const removeValue = () => {
+    setCounter(counter -1)
+  }
+
+  return (
+    <>
+      <h1>React course with hotesh {counter}</h1>
+      <h2>Counter value: {counter}</h2>
+      <button
+        onClick={addValue}
+        >Add value</button> {" "}
+      <button
+        onClick={removeValue}>Remove value</button>
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
